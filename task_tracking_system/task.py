f48c3340b5e4f0293fb2610452331622b40ce76a
Stanislau Klimovich <stasklimovich299@gmail.com>
2021-08-20 00:48:56 +0300

    MainTaskHandler class added

--- task_tracking_system/task.py
+++ task_tracking_system/task.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 
-class TaskMain:
+class Task:
     def __init__(self, name: str, description: str):
         self._name = name
         self._description = description
@@ -11,7 +11,7 @@ class TaskMain:
         return f"Task name: {self._name}\nTask description: {self._description}"
 
     def __repr__(self):
-        return self.__str__()
+        return str(self)
 
     def progress(self):
         if self.__state == "New":
@@ -23,8 +23,42 @@ class TaskMain:
         else:
             print('Task state is already "Done"')
 
+    def get_state(self):
+        return self.__state
+
 
 @dataclass
 class User:
     name: str
 
+
+class MainTaskHandler:
+    def __init__(self):
+        self.active_user = None
+        self.active_task = None
+        self.command = None
+        self.data = {}
+
+    def ask_for_command(self):
+        self.command = str(input('please, input a command: '))
+        return self.command
+
+    def create_user(self, name: str):
+        self.active_user = User(name)
+        self.data[self.active_user.name] = []
+
+    def create_task(self, user: str, task_name: str, task_description: str):
+        self.active_task = Task(name=task_name, description=task_description)
+        if user in self.data:
+            self.data[user].append(self.active_task)
+        else:
+            self.create_user(user)
+            self.create_task(user=user, task_name=task_name, task_description=task_description)
+
+
+if __name__ == '__main__':
+    handler = MainTaskHandler()
+    while True:
+        command = handler.ask_for_command()
+        if command.lower() == 'exit':
+            break
