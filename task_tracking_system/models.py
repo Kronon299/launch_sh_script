c4001c79072f79baf826501b66d41cee414891c8
Stanislau Klimovich <stasklimovich299@gmail.com>
2021-08-22 23:33:46 +0300

    models.py added

---
+++ task_tracking_system/models.py
@@ -0,0 +1,65 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import declarative_base, sessionmaker, scoped_session, relationship
+
+engine = create_engine("sqlite:///example.db")
+Base = declarative_base(bind=engine)
+
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), unique=True)
+
+    tasks = relationship("Task", back_populates="user")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.username!r})"
+
+    def __repr__(self):
+        return str(self)
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128), nullable=False)
+    description = Column(String(256), nullable=True)
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+
+    user = relationship(User, back_populates="tasks")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, title={self.title!r}, user={self.author})"
+
+    def __repr__(self):
+        return str(self)
+
+
+def create_user(session, username: str) -> User:
+    """
+    :param session:
+    :param username:
+    :return:
+    """
+
+    u = User(username=username)
+    print("id before:", u.id)
+    session.add(u)
+    session.commit()
+    print("id after:", u.id)
+    return u
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all()
+    session = Session()
+    try:
+        u = create_user(session, "sam")
+    except IntegrityError:
+        print('this user name already exists')
